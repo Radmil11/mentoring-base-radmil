65918f33fcaeb6ee8ba2d409eae35a7db5f0165f
Radmil Khasanov <aminasabirova@MacBook-Air--Radmil.local>
2024-10-23 19:33:26 +0400

    fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  readonly headerItem1 = 'Главная';
+
+  readonly headerItem2 = 'О компании';
+
+  readonly headerItem3 = 'Каталог';
+
+  readonly header2Item1 = 'Каталог';
+
+  readonly header2Item2 = 'Cтройматериалы';
+
+  readonly header2Item3 = 'Инструменты';
+
+  readonly header2Item4 = 'Электрика';
+
+  readonly header2Item5 = 'Интерьер и одежда';
+
+
 }
